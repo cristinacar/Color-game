991e9e15cfab1b355e8fb290a786624bace4e4b2
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-07-25 15:11:58 +0300

    added click event listener with jquery

--- color-game-try1/color1.js
+++ color-game-try1/color1.js
@@ -1,13 +1,25 @@
-window.addEventListener('keydown',function(e){
-  var key_circle = document.querySelector("div[data-key='" + e.keyCode + "']");
-  key_circle.classList.add('key1');
-  setTimeout(function(){
-    key_circle.classList.remove('key1');
-  }, 500);
+$(document).ready(function(){
+  
+  //keydown
+  window.addEventListener('keydown',function(e){
+    var key_circle = document.querySelector("div[data-key='" + e.keyCode + "']");
+    key_circle.classList.add('key1');
+    setTimeout(function(){
+      key_circle.classList.remove('key1');
+    }, 500);
 
-  console.log(key_circle);
+    console.log(key_circle);
+  });
+
+  //added the click event 
+  $('.color-circle').click(function(){
+    var theDiv = this;
+    $(theDiv).addClass('key1');
+    setTimeout(function(){
+      $(theDiv).removeClass('key1');
+    },500);
+  });
+
+
+
 });
-
-
-
-//add hover delay and then click
