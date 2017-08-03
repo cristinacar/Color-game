ed930c4e0e02bee58ac6a7cc4bb6f3692bf1bfd8
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-08-03 11:29:23 +0300

    added the click event listener on circle that calculates the difference between clicks and displays the circle on a random position

--- Reaction Testing Game/index.js
+++ Reaction Testing Game/index.js
@@ -1,5 +1,6 @@
 
 var circle = document.getElementById('circle');
+var diff = document.getElementById('diff');
 
 
 function randomVW(){
@@ -14,12 +15,27 @@ function randomVW(){
 function randomVH(){
   var vh = Math.floor(Math.random() * 101);
   if(vh >= 79){
-    return 'calc('+vh+'vh - 200px)';
+    return 'calc('+vh+'vh - 400px)';
   }
   return vh + 'vh';
 }
 
+var createdTime = Date.now();
 circle.addEventListener('click',function(){
   this.style.left = randomVW();
   this.style.top = randomVH();
+  var clickedTime = Date.now();
+  var seconds = (clickedTime - createdTime) / 1000;
+  console.log(seconds);
+  var reactionTime ='Your reaction is: ' + seconds + ' seconds';
+  if(seconds < 0.700) {
+    diff.textContent = 'Nice! Your reaction is: ' + seconds + ' seconds';
+  }else {
+    diff.textContent = 'Nice job, grandpa! Your reaction is: ' + seconds + ' seconds';
+  }
+  //diff.textContent = reactionTime;
+  createdTime = Date.now();
 });
+
+
+/*difference between mouse up and mouse down*/
