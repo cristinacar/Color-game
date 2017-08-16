cca7afce09d77ae49e1a710725b62943260d2467
Popa Irina <popairinamihaela@gmail.com>
2017-08-16 10:04:03 +0300

    update color-game (with different levels)

--- color-game/colorGame1.js
+++ color-game/colorGame1.js
@@ -1,7 +1,23 @@
-function loadColors(){
-	//setTransform seteaza div-urile sa fie animate cand mouse-ul este deasupra
-	setTransform();
-	document.getElementById("news").innerHTML="Good luck !";
+var nrIncercari=3;
+var nivel=6;
+
+function loadColors(incercari,nrDiv){
+	$("#sectionForColors").html("");
+	var htmlCode="";
+	for(i=1;i<=nrDiv;i++){
+		htmlCode+="<div id=\"box-"+i+"\" class=\"box\"  onclick=\"check("+i+")\" ></div>";
+
+	}
+	$("#sectionForColors").html(htmlCode);
+	if(incercari==3){
+		nivel=6;
+	}
+	/*	nr de incercari  */
+	console.log(incercari);
+	console.log("nivel"+nivel);
+	nrIncercari=incercari;
+	document.getElementById("news").innerHTML= "Numar incercari: " +nrIncercari;
+
 	var rgb=generateRGB();
 	document.getElementById("rgbColor").innerHTML=rgb;
 	
@@ -9,25 +25,26 @@ function loadColors(){
 	var colors=new Array();
 	colors.push(rgb);
 	var rgbNew;
-	for(i=1;i<=5;i++){
+	for(i=1;i<=nrDiv-1;i++){
 		rgbNew=generateRGB();
 		colors.push(rgbNew);
 	}
 
 	//fiindca intotdeauna 'super-culoarea' este pe prima pozitie din vector, alegem o pozitie random de la 1 la 6 pentru aceasta
-	var poz=Math.floor((Math.random() * 6) + 1);
+	var poz=Math.floor((Math.random() * nrDiv) + 1);
+	console.log("pozitia pe care se afla super-culoarea " +poz);
 	setColor(poz,colors[0]);
 
 	// vector iDs contine pozitiile pe care s-au setat culori
 	var iDs =new Array();
 	iDs.push(poz);
-	for(i=1;i<=5;i++){
+	for(i=1;i<=nrDiv-1;i++){
 		//se alege o noua pozitie
-		var newPoz=Math.floor((Math.random() * 6) + 1);
+		var newPoz=Math.floor((Math.random() * nrDiv) + 1);
 		//daca pozitia se afla in vectorul de pozitii, se genereaza incontinuu alte pozitii pana cand nu se vor intalni in vectorul iDs	
 		if(containsValue(iDs,newPoz)==1){
 			while(containsValue(iDs,newPoz)==1){
-				newPoz=Math.floor((Math.random() * 6) + 1);
+				newPoz=Math.floor((Math.random() * nrDiv) + 1);
 			}
 		}
 		setColor(newPoz,colors[i]);
@@ -73,17 +90,35 @@ function check(id){
 	var color=boxClicked.style.backgroundColor;
 	if(color.replace(/ /g,"")==rgb.replace(/ /g,"")){
 		document.getElementById("news").innerHTML="You guessed the color !";
-		$("#"+string).css("transform","rotate(45deg)");
+		nivel=nivel+3;
+		var x=Math.floor(nivel/2);
+		loadColors(x,nivel);
+		
 	}
 	else{
-		document.getElementById("news").innerHTML="Try again !";
+		nrIncercari--;
+		document.getElementById("news").innerHTML="Numar incercari: "+nrIncercari;
+		if(nrIncercari==0){
+			if(nivel==6){
+				loadColors(3,nivel);
+			}
+			if(nivel>6){
+				nivel=nivel-3;
+				var x=Math.floor(nivel/2);
+				loadColors(x,nivel);
+			}
+			//start new game -daca nivelul e 1
+			//se da cu un nivel inapoi altfel 
+
+		}
 		$("#"+string).css("transform","none");
 	}
 	
 }
+//functia nu mai este folosita
 function setTransform(){
-for(i=1;i<=6;i++){
-    	//console.log("hover",i);
+	console.log("intra");
+	for(i=1;i<=6;i++){  	
         $("#box-"+i).css("transform","");
         $("#box-"+i+":hover").css("transform","rotate(45deg)");
 	}
